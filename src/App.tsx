e1998390d5232e043d22b966b8be0a1986fa6dd6
yangjunyi <softenginejy@gmail.com>
2022-07-02 19:05:07 +0800

    feat(editor): 脚手架模板

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+  console.log('>>>>>>>>>>>>>>')
+  return (
+    <div>
+      {/* <Header></Header> */}
+      hello, world i am xxx 23144112221
+    </div>
+  );
+};
+
+export default App;
